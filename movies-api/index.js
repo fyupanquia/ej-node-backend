e74e3b85455041b50fe639542e6d5ded411409b0
fyupanquia <fyupanquia@outlook.com>
2020-06-06 18:43:30 -0500

    attempt2

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
-const path = require('path')
+//const fs = require('fs');
+//const path = require('path')
 const express = require('express');
 const app = express();
 
 const { config } = require('./config/index');
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+//const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
 
 const {
   logErrors,
@@ -15,7 +15,7 @@ const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 const moviesAPI = require("./routes/movies")
 const bodyParser = require('body-parser');
-const morgan = require("morgan")
+//const morgan = require("morgan")
 /*
 app.get('/', function(req, res) {
   res.send('hello world');
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false }));
 
 
-app.use(morgan('combined', { stream: accessLogStream }))
+//app.use(morgan('combined', { stream: accessLogStream }))
 
 
 moviesAPI(app)
@@ -44,7 +44,7 @@ app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, function() {
-  //console.log(`Listening http://localhost:${config.port}`);
-  const debug = require('debug')('app:server'); //ok
-  debug(`Listening http://localhost:${config.port}`)
+  console.log(`Listening http://localhost:${config.port}`);
+  //const debug = require('debug')('app:server'); //ok
+  //debug(`Listening http://localhost:${config.port}`)
 });
